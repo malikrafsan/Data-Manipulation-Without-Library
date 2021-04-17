f8b10b83c8ed8441443369e97917b56b1c421abd
Gerald Abraham Sianturi <68854343+geraldabrhm@users.noreply.github.com>
2021-04-17 16:53:42 +0700

    Update F4.py

--- F4.py
+++ F4.py
@@ -1,53 +1,67 @@
 def caritahun():
-    tahun = int(input("Masukkan tahun: "))
-    kategori = input("Masukkan kategori: ")
-    
-    print()
-    print("Hasil pencarian: ")
-    print()
-    
-    if kategori == "=":
-        for i in range(1,len(gadget)):
+  while True:
+      try:
+        tahun = int(input("Masukkan tahun: "))
+        kategori = input("Masukkan kategori: ")
+        found = False
+
+        print()
+        print("Hasil pencarian: ")
+        print()
+        
+        if kategori == "=":
+          for i in range(1,len(gadget)):
             if gadget[i][5] == tahun:
-                print(f"Nama: {gadget[i][1]}")
-                print(f"Deskripsi: {gadget[i][2]}")
-                print(f"Jumlah: {gadget[i][3]}")
-                print(f"Rarity: {gadget[i][4]}")
-                print(f"Tahun Ditemukan: {gadget[i][5]}")
-                print()
-    elif kategori == "<":
-        for i in range(1, len(gadget)):
+              print(f"Nama: {gadget[i][1]}")
+              print(f"Deskripsi: {gadget[i][2]}")
+              print(f"Jumlah: {gadget[i][3]}")
+              print(f"Rarity: {gadget[i][4]}")
+              print(f"Tahun Ditemukan: {gadget[i][5]}")
+              print()
+              found = True
+        elif kategori == "<":
+          for i in range(1, len(gadget)):
             if gadget[i][5] < tahun:
-                print(f"Nama: {gadget[i][1]}")
-                print(f"Deskripsi: {gadget[i][2]}")
-                print(f"Jumlah: {gadget[i][3]}")
-                print(f"Rarity: {gadget[i][4]}")
-                print(f"Tahun Ditemukan: {gadget[i][5]}")
-                print()
-    elif kategori == ">":
-        for i in range(1, len(gadget)):
+              print(f"Nama: {gadget[i][1]}")
+              print(f"Deskripsi: {gadget[i][2]}")
+              print(f"Jumlah: {gadget[i][3]}")
+              print(f"Rarity: {gadget[i][4]}")
+              print(f"Tahun Ditemukan: {gadget[i][5]}")
+              print()
+              found = True
+        elif kategori == ">":
+          for i in range(1, len(gadget)):
             if gadget[i][5] > tahun:
-                print(f"Nama: {gadget[i][1]}")
-                print(f"Deskripsi: {gadget[i][2]}")
-                print(f"Jumlah: {gadget[i][3]}")
-                print(f"Rarity: {gadget[i][4]}")
-                print(f"Tahun Ditemukan: {gadget[i][5]}")
-                print()
-    elif kategori == "<=":
-        for i in range(1, len(gadget)):
+              print(f"Nama: {gadget[i][1]}")
+              print(f"Deskripsi: {gadget[i][2]}")
+              print(f"Jumlah: {gadget[i][3]}")
+              print(f"Rarity: {gadget[i][4]}")
+              print(f"Tahun Ditemukan: {gadget[i][5]}")
+              print()
+              found = True
+        elif kategori == "<=":
+          for i in range(1, len(gadget)):
             if gadget[i][5] <= tahun:
-                print(f"Nama: {gadget[i][1]}")
-                print(f"Deskripsi: {gadget[i][2]}")
-                print(f"Jumlah: {gadget[i][3]}")
-                print(f"Rarity: {gadget[i][4]}")
-                print(f"Tahun Ditemukan: {gadget[i][5]}")
-                print()
-    elif kategori == ">=":
-        for i in range(1, len(gadget)):
+              print(f"Nama: {gadget[i][1]}")
+              print(f"Deskripsi: {gadget[i][2]}")
+              print(f"Jumlah: {gadget[i][3]}")
+              print(f"Rarity: {gadget[i][4]}")
+              print(f"Tahun Ditemukan: {gadget[i][5]}")
+              print()
+              found = True
+        elif kategori == ">=":
+          for i in range(1, len(gadget)):
             if gadget[i][5] >= tahun:
-                print(f"Nama: {gadget[i][1]}")
-                print(f"Deskripsi: {gadget[i][2]}")
-                print(f"Jumlah: {gadget[i][3]}")
-                print(f"Rarity: {gadget[i][4]}")
-                print(f"Tahun Ditemukan: {gadget[i][5]}")
-                print()
+              print(f"Nama: {gadget[i][1]}")
+              print(f"Deskripsi: {gadget[i][2]}")
+              print(f"Jumlah: {gadget[i][3]}")
+              print(f"Rarity: {gadget[i][4]}")
+              print(f"Tahun Ditemukan: {gadget[i][5]}")
+              print()
+              found = True
+        if found == False:
+              print("Tidak ada gadget yang ditemukan")
+      except ValueError:
+        print()
+      else:
+        break
